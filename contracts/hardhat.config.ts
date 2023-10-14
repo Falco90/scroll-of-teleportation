d8e5b5ae744abe41310799e80cb93792df20e5f9
Falco90 <falcorodenburg@gmail.com>
2023-10-14 21:58:26 +0700

    add hardhat config for verifying L1ERC721Gateway contract

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -5,6 +5,19 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  etherscan: {
+    apiKey: {
+      scrollSepolia: "D62920783A4311EE9D6600155D570C742E"
+    },
+    customChains: [{
+      network: "scrollSepolia",
+      chainId: 534351,
+      urls: {
+        apiURL: "https://api-sepolia.scrollscan.dev/api",
+        browserURL: "https://sepolia.scrollscan.dev"
+      }
+    }]
+  },
   networks: {
     scrollSepolia: {
       url: "https://sepolia-rpc.scroll.io/" || "",
